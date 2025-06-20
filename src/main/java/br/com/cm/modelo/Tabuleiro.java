a8ec84d5b04e5efce9df4630cf50df1b9dd1083a
alangonno <alannogueira314@gmail.com>
2025-06-20 16:08:50 -0300

    Sorteador de Minas e Vitoria

--- src/main/java/br/com/cm/modelo/Tabuleiro.java
+++ src/main/java/br/com/cm/modelo/Tabuleiro.java
@@ -2,6 +2,7 @@ package br.com.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 
@@ -39,6 +40,26 @@ public class Tabuleiro {
     }
     
     private void sortearMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado = campo -> campo.isMinado();
+        do {
+            minasArmadas = campos.stream().filter(minado).count();
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+
+        } while (minasArmadas < minas);
+    }
+
+    public boolean objetivoAlcancado() {
+        return campos.stream().allMatch(c -> c.objetivoAlcancado());
+    }
+
+    public void reiniciar() {
+        campos.stream().forEach(campo -> campo.reiniciar());
+        sortearMinas();
+    }
+
+    public String toString() {
 
     }
 }
